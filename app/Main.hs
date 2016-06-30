7d4767a2dab1370c47d115d637c35ecfcb9a4959
David Heath <heath.davidanthony@gmail.com>
2016-06-29 21:07:08 -0400

    main executes a term in a file

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,10 @@ import Lambda
 import System.Environment (getArgs)
 
 main :: IO ()
-main = do res <- run . concat <$> getArgs
-          case res of
-            Left p  -> print p
-            Right s -> putStrLn s
+main = do args <- getArgs
+          case args of
+            [f] -> do res <- run <$> readFile f
+                      case res of
+                        Left p  -> print p
+                        Right s -> putStrLn s
+            _   -> putStrLn "Wrong number of arguments"
